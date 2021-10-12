e803437d54cde60d85e6d96f46776ae4f301aaaa
Charles <63302440+Sarrus1@users.noreply.github.com>
2021-10-12 15:21:31 +0200

    Update styles.ts

--- frontend/src/pollColors/styles.ts
+++ frontend/src/pollColors/styles.ts
@@ -1,6 +1,6 @@
-﻿import { CSSProperties } from "react";
+import { CSSProperties } from "react";
 
 export const circleContainer: CSSProperties = {
   display: "flex",
-  height: "5rem",
+  height: "10vh",
 };
