a37a03b297335303d71de6cd2567338855d08bae
Sarrus1 <omegasix0611@gmail.com>
2021-12-05 21:02:53 +0100

    Fix: staticfiles not changing

---
+++ backend/start_server.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+cd /home/server/tensor
+
+echo "========= Migrate database"
+python3 manage.py migrate --no-input
+echo "========= DONE ============"
+
+echo "========= Collect static files"
+python3 manage.py collectstatic --no-input --clear
+echo "========= DONE ============"
+
+echo "========= Starting server ========="
+/usr/local/bin/gunicorn Tensor.wsgi:application --log-level info --log-file=- --name tensor -b 0.0.0.0:8000 --reload
