e13af2c9301233c665752408a50e2d7872a0fbfb
Sarrus1 <omegasix0611@gmail.com>
2022-10-02 01:20:57 +0200

    Chore: formatting

--- backend/tensor_site/models.py
+++ backend/tensor_site/models.py
@@ -4,19 +4,19 @@ from markdownx.models import MarkdownxField
 
 
 class News(models.Model):
-		title = models.CharField(max_length=100)
-		content = MarkdownxField()
-		date = models.DateField(default=timezone.now)
+    title = models.CharField(max_length=100)
+    content = MarkdownxField()
+    date = models.DateField(default=timezone.now)
 
-		def __str__(self):
-				return self.title
-		
-		class Meta:
-				verbose_name_plural = "News"
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "News"
 
 
 SERVER_CHOICES = [
-		('awp', 'AWP'),
-		('retakes', 'Retakes'),
-		('surf', 'Surf'),
-		]
+    ('awp', 'AWP'),
+    ('retakes', 'Retakes'),
+    ('surf', 'Surf'),
+]
